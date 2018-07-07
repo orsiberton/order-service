703c3cd22cbbc1d09510d3cc9d1f0fb0ac1e5891
Bruno Berton <bberton@ciandt.com>
2018-07-07 19:33:00 -0300

    fix swagger issue and add update order controller

--- order-command-api/src/main/java/com/ordercommand/ordercommandapi/config/SwaggerConfig.java
+++ order-command-api/src/main/java/com/ordercommand/ordercommandapi/config/SwaggerConfig.java
@@ -2,18 +2,15 @@ package com.ordercommand.ordercommandapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -22,23 +19,15 @@ public class SwaggerConfig {
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ordercommand.ordercommandapi.controller"))
-                .build();
+                .build()
+                .apiInfo(metaData());
     }
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
                 .title("API for generating Order commands ")
-                .description("\"REST API for generating Order commands\"")
+                .description("REST API for generating Order commands")
                 .version("1.0.0")
                 .build();
     }
-
-//    @Override
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
 }
--- order-command-api/src/main/java/com/ordercommand/ordercommandapi/controller/OrderController.java
+++ order-command-api/src/main/java/com/ordercommand/ordercommandapi/controller/OrderController.java
@@ -2,28 +2,50 @@ package com.ordercommand.ordercommandapi.controller;
 
 import com.ordercommand.ordercommandapi.controller.dto.OrderDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @Slf4j
-@RestController("/order")
+@Api("/order")
+@RestController
+@RequestMapping(value = "/order", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation(value = "Create an Order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Payload accepted for order creation."),
+            @ApiResponse(code = 500, message = "Something went wrong!") }
+    )
     public void createOrder(@RequestBody OrderDto orderDto) {
-        log.info("Order {} created!", orderDto.getOrderId());
+        log.info("Order {} will be created!", orderDto.getDescription());
     }
 
-//    @GetMapping
-//    public ResponseEntity getOk() {
-//        log.info("OK");
-//        return ResponseEntity.ok("OK");
-//    }
+    @PutMapping("/{orderId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation(value = "Update an Order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Payload accepted for order update."),
+            @ApiResponse(code = 500, message = "Something went wrong!") }
+    )
+    public void updateOrder(@ApiParam(value = "Order id to be updated", required = true) @PathVariable UUID orderId,
+                            @RequestBody OrderDto orderDto) {
+        log.info("Order {} will be updated!", orderId);
+    }
+
 }
